1221237a6554f658e448c36545ce58191b8b01a5
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-01 23:56:43 -0500

    feat: creation class Federacion

--- Federacion.cs
+++ Federacion.cs
@@ -8,9 +8,13 @@ public class Federacion
     public string CiudadOrigen {get; set;}
 
 
-    public Federacion (string){
-
+    public Federacion ( string _Nombre, int _Edad, string _CiudadOrigen){
+        this.Id =Guid.NewGuid().ToString();
+        this.Nombre = _Nombre;
+        this.Edad = _Edad;
+        this.CiudadOrigen = _CiudadOrigen;
     }
 
+   
 
 }
