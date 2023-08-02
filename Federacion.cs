ba31784cb066dc3206f8ad36414397c65c504c27
Giovanny Moreno <44243879+Giovannymo@users.noreply.github.com>
2023-08-01 22:50:42 -0500

    First Commit

---
+++ Federacion.cs
@@ -0,0 +1,16 @@
+namespace Federacion_Ejercicio;
+
+public class Federacion
+{
+    public string Id {get;  set;}
+    public string Nombre {get; set;}
+    public int Edad {get; set;}
+    public string CiudadOrigen {get; set;}
+
+
+    public Federacion (string){
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        bool next = true;
+
+
+        do{
+            int Option = 0;
+            switch(Option){
+                case 0: 
+                    next = false;             
+                break;
+            }
+
+        }while(next);
+
+
+    }
+}
