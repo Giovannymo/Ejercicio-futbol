8826fac8c96bec5e32bb54df07576475c6c033f4
Giovanny Moreno <giovannysocas@gmail.com>
2023-08-01 23:59:14 -0500

    test objects in Program class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿internal class Program
+﻿namespace Federacion_Ejercicio;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-
+        Jugador jugador= new Jugador("Milthon",22,"Floridablanca","Delantero",9);
         bool next = true;
-
+        jugador.Entrenar();
+        jugador.Salud();
+        jugador.CambioPosicion("arquero");
 
         do{
             int Option = 0;
